3ab589634d583d5fdda3437764b4b4c78d7795f5
alanlicz <alanltycz@icloud.com>
2020-04-23 17:13:27 -0700

    add floor

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,6 +2,11 @@ extends KinematicBody2D
 class_name Actor
 
 
-func _physics_process(delta: float) -> void:
-	var velocity: = Vector2(300, 0)
-	move_and_slide(velocity)
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
