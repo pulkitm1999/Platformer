b005a0b495d192d2a1d8123696652115528c81af
alanlicz <alanltycz@icloud.com>
2020-04-23 18:10:16 -0700

    add controls

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,11 +2,9 @@ extends KinematicBody2D
 class_name Actor
 
 
-export var speed: = Vector2(300.0, 1000.0)
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 200.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
-
-func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
